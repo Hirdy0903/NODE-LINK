096ecfcddf4bdfdf9ab4cacaca4e4cdb5abd7e5e
Hirdyansh <hirdyanshsaxena7@gmail.com>
2026-06-01 20:41:43 +0530

    initialised graphql server

---
+++ build/index.js
@@ -0,0 +1,33 @@
+import { ApolloServer } from '@apollo/server';
+import express from 'express';
+import { expressMiddleware } from '@as-integrations/express5';
+async function startServer() {
+    const port = Number(process.env.PORT) || 3000;
+    const app = express();
+    app.use(express.json()); //parse json body 
+    //create graphql server
+    const server = new ApolloServer({
+        typeDefs: `
+    type Query {
+      hello: String
+      say(name:String!): String 
+    }
+  `,
+        resolvers: {
+            Query: {
+                hello: () => 'hello i am graphql server',
+                say: (_, { name }) => `hello ${name} from graphql server`
+            }
+        }
+    });
+    await server.start();
+    app.use('/graphql', expressMiddleware(server));
+    app.get('/', (req, res) => {
+        res.json({ message: 'Hello World' });
+    });
+    app.listen(port, () => {
+        console.log('server started at', port);
+    });
+}
+startServer();
+//# sourceMappingURL=index.js.map
